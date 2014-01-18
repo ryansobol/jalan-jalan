b96b7a011faea29a1e6216468a7bb20aab5f34b6
Ryan Sobol <contact@ryansobol.com>
2014-01-18 14:01:51 +0800

    Close #1 Initialize an empty Xcode project

---
+++ Jalan Jalan/main.m
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "JLNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JLNAppDelegate class]));
+    }
+}
